40a22dd2a13eb04dd9b1b00da574d7b23be43fab
Harshmahto <62500223+Harshmahto@users.noreply.github.com>
2024-03-23 22:38:18 +0530

    refreshtoken endpoint

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser } from "../controllers/user.controllers.js";
+import { loginUser, logoutUser, registerUser ,refreshAccessToken } from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 // import { verify } from "jsonwebtoken";
@@ -22,8 +22,9 @@ router.route("/register").post(
 )
 
 router.route("/login").post(loginUser)
-
+//secured routs
 router.route("/logout").post(verifyJWT,  logoutUser)
+router.route("/refresh-token").post(refreshAccessToken)
 
 
 export default router;
