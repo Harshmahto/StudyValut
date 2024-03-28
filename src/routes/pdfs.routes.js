4967e6559e038fe46a4f8165a9f9faf6bc2943fe
Harshmahto <62500223+Harshmahto@users.noreply.github.com>
2024-03-28 14:54:41 +0530

    route

--- src/routes/pdfs.routes.js
+++ src/routes/pdfs.routes.js
@@ -4,10 +4,9 @@ import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router();
 
-/*router.route("/uploadpdf").post(upload.single({
-    name:"pdfFile",
-    maxcount:1
-}),verifyJWT,uploadPDF)*/
+
+//secured routes
+
 router.route("/uploadpdf").post(upload.single('pdfFile'),verifyJWT,uploadPDF)
 
 
